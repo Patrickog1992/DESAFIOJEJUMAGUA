8cc5f5b356ffdaba9721c14a28375f04e2096728
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-09-09 13:52:41 +0000

    Eu te falei para mexer somente no quizz masculino e no quizz depoimento

--- src/components/testimonial-page.tsx
+++ src/components/testimonial-page.tsx
@@ -26,33 +26,33 @@ export function TestimonialPage({ onContinue }: TestimonialPageProps) {
       <CardContent className="space-y-4">
         <div className="flex justify-center items-center gap-2 sm:gap-4">
           <Image
-            src="https://v3.certifiedfasting.com/pt-pt/g-22m-eur/img/0lTIBNHU7R-734.webp"
-            alt="Depoimento de Roberto Alves antes"
+            src="https://v3.certifiedfasting.com/pt-pt/g-22m-eur/img/u5uM71C9tF-734.webp"
+            alt="Depoimento de Joana Sousa antes"
             width={140}
             height={140}
             className="rounded-lg border-4 border-muted object-contain w-[140px] h-[140px] sm:w-[200px] sm:h-[200px]"
-            data-ai-hint="man before weight loss"
+            data-ai-hint="woman before weight loss"
           />
           <Image
-            src="https://v3.certifiedfasting.com/pt-pt/g-22m-eur/img/GF1sQ1rSir-734.webp"
-            alt="Depoimento de Roberto Alves depois"
+            src="https://v3.certifiedfasting.com/pt-pt/g-22m-eur/img/x5YnU5g6rE-734.webp"
+            alt="Depoimento de Joana Sousa depois"
             width={140}
             height={140}
             className="rounded-lg border-4 border-primary object-contain w-[140px] h-[140px] sm:w-[200px] sm:h-[200px]"
-            data-ai-hint="man after weight loss"
+            data-ai-hint="woman after weight loss"
           />
         </div>
         <div className="bg-accent/50 p-4 rounded-lg">
           <CardTitle className="text-xl mb-2">
-            Perdeu 15 kg em 2 meses
+            Perdeu 12 kg em 2 meses
           </CardTitle>
           <CardDescription className="text-base text-foreground">
             "Sempre lutei com o meu peso, mas este plano tornou tudo mais fácil.
-            Em apenas dois meses, perdi 15 kg e nunca me senti tão bem! Tenho
+            Em apenas dois meses, perdi 12 kg e nunca me senti tão bem! Tenho
             mais energia e confiança. Recomendo a todos que querem uma mudança
             real e sustentável."
           </CardDescription>
-          <p className="font-bold mt-4">- Roberto Alves</p>
+          <p className="font-bold mt-4">- Joana Sousa</p>
         </div>
       </CardContent>
       <CardFooter className="justify-center">
