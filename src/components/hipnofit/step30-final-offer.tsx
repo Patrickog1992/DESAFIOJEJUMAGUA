1d3888475b6a87140562bcec0a24973d9180d9ec
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-10-08 15:37:53 +0000

    você consegue deixar esse audio em um estilo minimalista sem ser de what

--- src/components/hipnofit/step30-final-offer.tsx
+++ src/components/hipnofit/step30-final-offer.tsx
@@ -98,7 +98,7 @@ export function Step30_FinalOffer({ data }: Props) {
                     <CardDescription>Aperte o play e escute esse aúdio em um lugar tranquilo</CardDescription>
                 </CardHeader>
                 <CardContent className="flex justify-center p-6">
-                   <div style={{position: 'relative', width: '100%', height: '0', paddingTop: '56.25%', paddingBottom: '0', boxShadow: '0 2px 8px 0 rgba(63,69,81,0.16)', marginTop: '1.6em', marginBottom: '0.9em', overflow: 'hidden', borderRadius: '8px', willChange: 'transform'}}>
+                   <div style={{position: 'relative', width: '100%', maxWidth: '500px', height: '0', paddingTop: '15%', paddingBottom: '0', boxShadow: '0 2px 8px 0 rgba(63,69,81,0.16)', marginTop: '1.6em', marginBottom: '0.9em', overflow: 'hidden', borderRadius: '8px', willChange: 'transform'}}>
                         <iframe loading="lazy" style={{position: 'absolute', width: '100%', height: '100%', top: '0', left: '0', border: 'none', padding: '0', margin: '0'}} src="https://www.canva.com/design/DAG1N7k1i8Y/dWFu8xnOeXV9ArZoLfFyMA/watch?embed" allowFullScreen={true} allow="fullscreen"></iframe>
                    </div>
                 </CardContent>
